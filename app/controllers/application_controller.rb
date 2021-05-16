43358fdd6c10451dd3cc2352da6da700bcc7b9ad
takaponmonogatari <taka11hiro26@gmail.com>
2021-05-16 22:34:33 +0000

    [Update]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def after_sign_in_path_for(resource)
@@ -11,8 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permitted(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
-
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  has_many :books,dependent: :destroy
-  attachment :profile_image
+
+  has_many :books
+  attachment :profile_image, destroy: false
+
+  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
 end
