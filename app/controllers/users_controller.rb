cdd6888e66349e52a06f6f3cd890d24fa915b82f
takaponmonogatari <taka11hiro26@gmail.com>
2021-05-30 23:24:59 +0000

    [Update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,14 @@ class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
+    @user = @user.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def index
+    @users = User.all
+    @book = Book.new
   end
 
   def edit
@@ -13,6 +18,10 @@ class UsersController < ApplicationController
 
   def updated
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:introuction,:profile_image)
 
 
 end
