2438a2073a2525ff88d38ef4798ede2c7ab5ba12
takaponmonogatari <taka11hiro26@gmail.com>
2021-06-01 23:54:19 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "homes#top"
-  get "home/about" => "homes#about"
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root :to => "homes#top"
+  get "home/about" => "homes#about"
+
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
+  end
 end
